4c34ad22bb24cb64629c4e95a30a993764bb7dc3
N-DiLo <egbekwunwanedilobu@gmail.com>
2023-09-10 17:53:12 +0100

    feat: added border color to app

--- lib/shared/app_colors.dart
+++ lib/shared/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 const kcPrimaryColor = Color(0xff4CA6A8);
 const kcTextColor = Color(0xff1A1D1E);
 const kcGreyColor = Color(0xff6A6A6A);
-const kcBackgroundColor = Color(0xffE1E1E1);
+const kcBorderColor = Color(0xffE2E2E2);
+const kcBackgroundColor = Color(0xffF8F8F8);
